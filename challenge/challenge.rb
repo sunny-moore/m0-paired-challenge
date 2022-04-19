938a9f708f7eb0cb561c6c04e760aec1da8bbf44
Sunny Moore <moore.sunny@gmail.com>
2022-04-18 19:29:35 -0700

    Initial Commit

---
+++ challenge/challenge.rb
@@ -0,0 +1,8 @@
+have_read = true
+pages = 532
+title = "Mistborn"
+author = "Brandon Sanderson"
+audiobook_length = 632.5
+isbn = "12345678"
+
+books =["Mistborn", "Way of Kings", "Oathbringer"]
---
+++ challenge/index.md
@@ -0,0 +1,6 @@
+# List of Books
+1. Mistborn
+2. The Stand
+3. Born a Crime
+4. The Gutter Prayer
+5. Harry Potter
